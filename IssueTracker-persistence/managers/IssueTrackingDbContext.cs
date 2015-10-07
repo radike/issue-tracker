635656fea94226533cb0b01b1d03e82c8fd2d675
radike <radoslavrabara@gmail.com>
2015-10-07 19:02:51 +0200

    Created User entity and UserManager

---
+++ IssueTracker-persistence/managers/IssueTrackingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IssueTracker_persistence.entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTracker_persistence.managers
+{
+    public class IssueTrackerDbContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
