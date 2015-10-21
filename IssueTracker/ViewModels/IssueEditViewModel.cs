d332c3f6394c0614b8c737ee8020fa524c9fb005
radike <radoslavrabara@gmail.com>
2015-10-21 15:34:07 +0200

    Fixed IssueEditViewModel to require Name and Project

--- IssueTracker/ViewModels/IssueEditViewModel.cs
+++ IssueTracker/ViewModels/IssueEditViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace IssueTracker.ViewModels
 {
@@ -6,10 +7,14 @@ namespace IssueTracker.ViewModels
     {
         // Id
         public Guid Id { get; set; }
-        
+
         // Issue editable fields
+        [Required]
         public Guid ProjectId { get; set; }
+
         public Guid AssigneeId { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
     }
