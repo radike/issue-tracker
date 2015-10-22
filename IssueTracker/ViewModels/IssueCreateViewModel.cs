7b01ca212b87712a004949a765da55cb4eb163fe
radike <radoslavrabara@gmail.com>
2015-10-22 09:29:42 +0200

    Added missing IssueCreateViewModel class
    - fixes previous commit a49261579eb5de2144de135841d64ac265e06105

---
+++ IssueTracker/ViewModels/IssueCreateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IssueTracker.ViewModels
+{
+    public class IssueCreateViewModel
+    {
+        [Required]
+        public Guid ProjectId { get; set; }
+
+        public string AssigneeId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
