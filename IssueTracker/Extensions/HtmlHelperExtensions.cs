b2ccd02a498311b8d5af345a341b31458235a65d
Peter Petrinec <petrinecp@gmail.com>
2015-11-14 11:22:07 +0100

    Fix some cosmetic issues #17.

---
+++ IssueTracker/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IssueTracker.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string ActivePage(this HtmlHelper helper, string controller, string action)
+        {
+            string classValue = "";
+
+            string currentController = helper.ViewContext.Controller.ValueProvider.GetValue("controller").RawValue.ToString();
+            string currentAction = helper.ViewContext.Controller.ValueProvider.GetValue("action").RawValue.ToString();
+
+            if (currentController == controller && currentAction == action)
+            {
+                classValue = "gn-selected";
+            }
+
+            return classValue;
+        }
+    }
+}
