304d1927452c76a4f87f41879fca48e2633f3eff
radike <radoslavrabara@gmail.com>
2015-12-14 14:18:04 +0100

    Removed commented-out code from StateRepository#61

--- IssueTracker.Data/Data Repositories/StateRepository.cs
+++ IssueTracker.Data/Data Repositories/StateRepository.cs
@@ -13,37 +13,10 @@ namespace IssueTracker.Data.Data_Repositories
     public class StateRepository : DataRepositoryBase<State>, IStateRepository
     {
         public StateRepository(IDbContext context)
-            :base(context)
+            : base(context)
         {
 
         }
-        //protected override State AddEntity(IssueTrackerContext entityContext, State entity)
-        //{
-        //    return entityContext.States.Add(entity);
-        //}
-
-        //protected override IEnumerable<State> GetEntities(IssueTrackerContext entityContext)
-        //{
-        //    return from e in entityContext.States.Include("Issues")
-        //           select e;
-        //}
-
-        //protected override State GetEntity(IssueTrackerContext entityContext, Guid id)
-        //{
-        //    var query = (from e in entityContext.States.Include("Issues")
-        //                 where e.Id == id
-        //                 select e);
-        //    var results = query.FirstOrDefault();
-
-        //    return results;
-        //}
-
-        //protected override State UpdateEntity(IssueTrackerContext entityContext, State entity)
-        //{
-        //    return (from e in entityContext.States
-        //            where e.Id == entity.Id
-        //            select e).FirstOrDefault();
-        //}
 
         public IEnumerable<State> GetStatesOrderedByIndex()
         {
